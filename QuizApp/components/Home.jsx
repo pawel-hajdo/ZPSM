15802cbebddd6f905bdb43261e44848f996463e1
Paweł Hajdo <pawel.hajdo001@gmail.com>
2023-11-27 21:17:18 +0100

    created QuestionCardView component added scrollview to home component

--- QuizApp/components/Home.jsx
+++ QuizApp/components/Home.jsx
@@ -1,11 +1,53 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {Button, ScrollView, StyleSheet, Text, View} from "react-native";
 import Header from "./Header";
+import QuestionCardView from "./QuestionCardView";
 
+const Tests = [
+    {
+        title: "Test1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+    {
+        title: "Test2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+    {
+        title: "Test3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+    {
+        title: "Test4",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+    {
+        title: "Test5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+    {
+        title: "Test6",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+    {
+        title: "Test7",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+    {
+        title: "Test8",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer gravida eros eros, ut eleifend dui ullamcorper vel. Nullam eget neque eu eros efficitur ullamcorper. Donec augue neque, accumsan ut facilisis vehicula, volutpat quis odio. Donec nec tincidunt nibh. Sed molestie cursus tellus, in ultricies metus varius dignissim. Sed a tincidunt metus."
+    },
+]
 function Home(){
     return (
         <View style={styles.container}>
             <Header text = "Home Page"/>
+            <ScrollView style={styles.scrollView}>
+                {Tests.map((test) => <QuestionCardView title = {test.title} description = {test.description}/> )}
+            </ScrollView>
+            <View style={styles.bottomContent}>
+                <Text style={{fontSize: 16}}>Get to know your ranking result</Text>
+                <Button title="Check!"/>
+            </View>
         </View>
     );
 }
@@ -14,6 +56,14 @@ const styles = StyleSheet.create({
     view: {
         flex: 1,
         justifyContent: 'center',
-    }
+    },
+    scrollView:{
+        height: '80%'
+    },
+    bottomContent: {
+        padding: 16,
+        backgroundColor: 'lightgray',
+        alignItems: 'center',
+    },
 });
 export default Home;
---
+++ QuizApp/components/QuestionCardView.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {View, StyleSheet, Text} from "react-native";
+
+const QuestionCardView = (params) => {
+    const getTitle = () => {
+        return params.title;
+    }
+
+    const getDescription = () => {
+        return params.description;
+    }
+
+    return (
+        <View style={styles.card}>
+            <View style={styles.cardContent}>
+                <Text style={styles.title}>{getTitle()}</Text>
+                <Text style={styles.description}numberOfLines={2}>{getDescription()}</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        borderRadius: 8,
+        overflow: "hidden",
+        margin: 10,
+        elevation: 3, // Elevation działa tylko na Androidzie
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+    },
+    cardContent: {
+        padding: 15,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    description: {
+        fontSize: 14,
+        color: "#666",
+        marginTop: 8,
+    },
+});
+export default QuestionCardView;
